cc3dc78a562060f1965d87a11ce54fb92c1e4ddc
Rahul <r4hulr@protonmail.com>
2026-04-16 12:17:59 +0530

    Add socket communication for vote requests in RaftNode class

--- index.py
+++ index.py
@@ -1,10 +1,15 @@
 import time
 import random
 import threading
+import socket
+import json
+
+
 class RaftNode:
     Host = '127.0.0.1'
     PORT = {i:5000+i for i in range(10)}
     def __init__(self, node_id,nei:dict,current_term=0,):
+        self.lock = threading.Lock()
         self.node_id = node_id
         self.current_term = current_term
         self.state = "follower"
@@ -15,8 +20,18 @@ class RaftNode:
         self.nei = nei
         self.election_timemout = random.randint(150,300)/1000
         self.last_heartbeat = time.time()
+      
     def recieve_AppendEntriesRPC(self, last_heartbeat:time):
         self.last_heartbeat =last_heartbeat
+    
+    def send_vote_request(self):
+        with self.lock:
+            data = {"term":self.current_term,"candidateId":self.node_id,"lastLogIndex":len(self.log)-1, "lastLogTerm":self.log[len(self.log)-1]["term"] if len(self.log) != 0 else 0}
+        payload = json.dumps(data).encode("utf-8")
+        for ne in self.nei:
+            with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock: 
+                sock.connect((self.Host,self.PORT[ne]))
+                sock.sendall(payload)
 
 
     def watchdog(self):
