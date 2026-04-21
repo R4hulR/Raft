d176f9a5ecae361bd1f247c889969d72a165b0f7
Rahul <r4hulr@protonmail.com>
2026-04-21 15:33:00 +0530

    Implement heartbeat and append entries handling in RaftNode class

--- index.py
+++ index.py
@@ -21,20 +21,26 @@ class RaftNode:
         self.election_timemout = random.randint(150,300)/1000
         self.last_heartbeat = time.time()
         self.N = len(self.nei) + 1
+        self.leaderID = None
     
     def send_heartbeat(self):
-        pass
+        for ne in self.nei:
+            with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock: 
+                sock.connect((self.Host,self.PORT[ne]))
+                with self.lock:
+                    response = {
+                        "type" : "AppendEntries",
+                        "term" : self.current_term,
+                        "leaderID" : self.node_id,
+                        "entries":[]
+                    }
+                sock.send(json.dumps(response).encode("utf-8"))
     def Leader(self):
         self.nextIndex = {ne: len(self.log) + 1 for ne in self.nei}
         self.matchIndex = {ne: 0 for ne in self.nei}
         while self.state == "Leader":
             self.send_heartbeat()
             time.sleep(50/1000)
-
-        
-    def recieve_AppendEntriesRPC(self, last_heartbeat:time):
-        self.last_heartbeat =last_heartbeat
-    
     
     def send_vote_request(self):
         with self.lock:
@@ -57,8 +63,52 @@ class RaftNode:
                         self.state="Leader"
                         threading.Thread(target=self.Leader,daemon=True).start()
                         break
-   
-    def receive_vote(self):
+    def handle_append_entries(self,conn,payload):
+        #Handle request directly from client
+        #I Think we need to make Payload["type"] as dictionary , so  payload["type"]["AppendEntries"] == "Leader" if we make "AppendEntries":"Client" or AppendEntries:Server
+        if payload["type"]["AppendEntries"] == "Client" and self.state!="Leader":
+            #redirect it to Leader
+            pass
+        #Handle request from Leader
+        # Receiver implementation:
+        # 1. Reply false if term < currentTerm (§5.1)
+        # 2. Reply false if log doesn’t contain an entry at prevLogIndex
+        # whose term matches prevLogTerm (§5.3)
+        # 3. If an existing entry conflicts with a new one (same index
+        # but different terms), delete the existing entry and all that
+        # follow it (§5.3)
+        # 4. Append any new entries not already in the log
+        # 5. If leaderCommit > commitIndex, set commitIndex =
+        # min(leaderCommit, index of last new entry)
+        else:
+            #Hanlde empty entries
+            if payload["term"] >= self.current_term:
+                with self.lock:
+                    if self.state == "Candidate":
+                        self.state = "Follower"
+                    self.current_term=payload["term"]
+                    self.last_heartbeat = time.time()
+                    response= {"success":True,"term":self.current_term}
+                    conn.send(json.dumps(response).encode("utf-8"))
+                return True
+
+
+    def handle_vote_request(self,payload,conn):
+        if payload["term"] < self.current_term:
+                    response= {"Id":self.node_id,"Voted":False}
+                    conn.send(json.dumps(response).encode('utf-8'))
+                    conn.close()
+                    return
+        with self.lock:
+            if self.votedFor == None or self.votedFor == payload["candidateId"]:
+                if payload["lastLogIndex"]>=len(self.log) and payload["lastLogTerm"]>=self.log[len(self.log)-1]["term"] if len(self.log) != 0 else 0:
+                    response = {"Id":self.node_id,"Voted":True}
+                    self.votedFor= payload["candidateId"]
+                    conn.send(json.dumps(response).encode('utf-8'))
+                else:
+                    response= {"Id":self.node_id,"Voted":False}
+                    conn.send(json.dumps(response).encode('utf-8'))
+    def server(self):
          with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
             sock.bind((self.Host,self.PORT[self.node_id]))
             sock.listen(128)
@@ -66,21 +116,12 @@ class RaftNode:
                 conn,addr = sock.accept()
                 payload = conn.recv(65552).decode('utf-8')
                 payload = json.loads(payload)
-                if payload["term"] < self.current_term:
-                    response= {"Id":self.node_id,"Voted":False}
-                    conn.send(json.dumps(response).encode('utf-8'))
-                    conn.close()
-                    continue
-                with self.lock:
-                    if self.votedFor == None or self.votedFor == payload["candidateId"]:
-                        if payload["lastLogIndex"]>=len(self.log) and payload["lastLogTerm"]>=self.log[len(self.log)-1]["term"] if len(self.log) != 0 else 0:
-                            response = {"Id":self.node_id,"Voted":True}
-                            self.votedFor= payload["candidateId"]
-                            conn.send(json.dumps(response).encode('utf-8'))
-                        else:
-                            response= {"Id":self.node_id,"Voted":False}
-                            conn.send(json.dumps(response).encode('utf-8'))
+                if payload["type"] == "vote": 
+                    self.handle_vote_request(payload,conn)
+                elif payload["type"] == "AppendEntries":
+                    self.handle_append_entries(conn,payload)
                 conn.close()
+
 
             
                 
